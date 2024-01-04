f15317d1cb2fa75266bf3cec659f9e5fe9fabc6f
pr0m3th3usEx <thomas1.michel@epitech.eu>
2024-01-05 00:56:42 +0100

    add tailwind configuration

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
